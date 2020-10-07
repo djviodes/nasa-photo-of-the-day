ea3d7ec14317c41e6eb1d543bd2f39ed54bab94c
djviodes <djviodes26@gmail.com>
2020-10-07 14:00:54 -0700

    Added files. Going to make App.js more React friendly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios'
+import Title from './components/Title'
+import Image from './components/Image'
 import "./App.css";
 
 function App() {
+  const [nasaInfo, setNasaInfo] = useState([])
+
+  useEffect(() => {
+    const fetchNasa = () => {
+      axios.get(`https://api.nasa.gov/planetary/apod?api_key=PFFF3FbuFIPjvr359uIXuqLYUYkcoEeyqoQOn7gu&date=2017-06-25`)
+        .then(res => {
+          setNasaInfo(res.data);
+        })
+        .catch(err => {
+          debugger
+        })
+    }
+    fetchNasa()
+  }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <Title />
+      <Image />
     </div>
   );
 }
