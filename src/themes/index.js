f934fba51c5740997e4c487889478527f2d30e0f
djviodes <djviodes26@gmail.com>
2020-10-08 13:44:27 -0700

    Added my index.js for the styled components

---
+++ src/themes/index.js
@@ -0,0 +1,24 @@
+export default {
+    primaryColor: 'royalblue',
+    secondaryColor: 'crimson',
+    tertiaryColor: 'mediumseagreen',
+    dangerColor: 'orange',
+    black: 'black',
+    white: 'white',
+    breakpoints: {
+        mobile: '(max-width: 550px)',
+        tablet: '(max-width: 800px)'
+    },
+    padding: {
+        small: '4px',
+        medium: '8px',
+        large: '12px',
+        xlarge: '16px',
+    },
+    margin: {
+        small: '4px',
+        medium: '8px',
+        large: '12px',
+        xlarge: '16px',
+    }
+}
